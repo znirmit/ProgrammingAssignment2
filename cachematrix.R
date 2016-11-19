c94357881a274b6199513af52d1916eb3b9ea580
Nirmit <znirmit@gmail.com>
2016-11-19 22:01:17 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+## The Cached Matrix is defined by this function for the further use.
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  ## Here now the function will be set to values of x,m in parent env.
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  ## Here get function is used to retrive x from parent env.
+  get <- function() x
+  ## Here setinv is used to set the value of m from parent env.
+  setinv <- function(solve) m <<- solve
+  ## Here getinv function is used to retrive m from parent env.
+  getinv <- function() m
+  ## Here list is used to return the named functions.
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse matrix that is cached 
+## by previous function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  ## Here if loop is used to retrive the value of m from cache if not null.
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  ## Here data is assigned get function as defined in previous function. 
+  data <- x$get()
+  ## Here m is assigned inverse of 'x'matrix
+  m <- solve(data, ...)
+  ## Here setinv function is retrived, as defined in previous function.
+  x$setinv(m)
+  ## m is returned
+  m
 }
